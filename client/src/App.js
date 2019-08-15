05268350c3f8ae197e1cd4c88201eb05e19347c5
Agnieszka Matyja <agmatyja@gmail.com>
2019-08-16 00:28:35 +0200

    app.js modification

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MainLayout from './components/layout/MainLayout/MainLayout';
+
+// import routes
+import Home from './components/pages/Home/HomePage';
+import SingleProduct from './components/pages/SingleProduct/SingleProductPage';
+import AddProduct from './components/pages/AddProduct/AddProduct';
+import Products from './components/pages/Products/ProductsPage';
+import Contact from './components/pages/Contact/ContactPage';
+import NotFound from './components/pages/NotFound/NotFoundPage';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <MainLayout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/posts" exact component={Products} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/products/new" exact component={AddProduct} />
+          <Route path="/products/:id" exact component={SingleProduct} />
+          <Route component={NotFound} />
+        </Switch>
+      </MainLayout>
+    );
+  }
+
 }
 
 export default App;
