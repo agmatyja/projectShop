7b6e87e2b14786fd673d29ae7a12d94c21082cf2
Agnieszka Matyja <agmatyja@gmail.com>
2019-08-27 18:01:32 +0200

    FAQ page formatted

--- client/src/components/pages/FAQ/FAQPage.js
+++ client/src/components/pages/FAQ/FAQPage.js
@@ -5,17 +5,16 @@ const FAQPage = () => (
   <div> 
     <h1>Frequently Asked Questions</h1>
 
-    <p>The discount code is "promo" :-)</p>
+    <h3>What is the discount code?</h3>
+    <p>"promo" :-)</p>
     <br />
 
-    <p>Lorem ipsum dolor sit amet?</p> 
+    <h3>Lorem ipsum dolor sit amet?</h3> 
     <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
     <br />
 
-    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur?</p> 
+    <h3>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur?</h3> 
     <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <br />
-
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br /> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
 );
