130cdb529dcab60b738ee8bc2ed72761f04deb37
Agnieszka Matyja <agmatyja@gmail.com>
2019-08-27 04:24:59 +0200

    links on the contact page

--- client/src/components/pages/Contact/ContactPage.js
+++ client/src/components/pages/Contact/ContactPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import "./ContactPage.scss";
 
 const ContactPage = () => (
-  <div>
-    <h1>Contact</h1>
+  <div className="contact">
+    <h1>Author</h1>
 	<p>Agnieszka Matyja</p>
-    
+	<ul>
+	<li><a href="https://www.linkedin.com/in/agnieszka-matyja-405623132/">LinkedIn</a></li>
+	<li><a href="https://github.com/agmatyja">GitHub</a></li>
+	</ul>
   </div>
 );
 
